6b73245f1fed6358512be614789298f8c0c27265
ogihaya <ogihaya1214@icloud.com>
2025-12-18 16:04:16 +0900

    いらないファイルを削除

--- GeoAlarm/GeoAlarmApp.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-@main
-struct GeoAlarmApp: App {
-    @StateObject private var store = AlarmStore()
-    @StateObject private var locationService = LocationService()
-    @StateObject private var notificationService = NotificationService()
-    @StateObject private var soundService = SoundService()
-}
